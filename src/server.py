3106674ae05229682837654eb2e36a9eba1ec2e6
lucianobajr <luciano.alcantara@ufv.br>
2023-06-30 14:03:28 -0300

    add

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ import os
 import random
 import string
 
-from flask import Flask, request, jsonify, send_from_directory,Response
+from flask import Flask, request, jsonify, send_from_directory, Response
 from werkzeug.utils import secure_filename
 
 from dotenv import load_dotenv
@@ -20,6 +20,7 @@ load_dotenv('.env')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'tmp/uploads'
 app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'jpeg', 'png', 'gif'])
+
 
 def generate_random_filename(filename):
     random_string = ''.join(random.choices(
@@ -49,6 +50,7 @@ def upload_s3(file, filename):
     s3_client.upload_fileobj(file, s3_bucket, filename)
     file_url = f'https://{s3_bucket}.s3.amazonaws.com/{filename}'
     return file_url
+
 
 @app.route('/posts', methods=['POST'])
 def create_post():
@@ -235,38 +237,71 @@ def ingredients_recipe():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/posts/classifier', methods=['POST'])
+def create_post_ai():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+
+    filename = generate_random_filename(secure_filename(file.filename))
+    upload_option = os.getenv('UPLOAD_CONFIG_DEVELOPMENT')
+    file_url = None
+
+    if upload_option == 'local':
+        file_url = upload_local(file, filename)
+    elif upload_option == 's3':
+        file_url = upload_s3(file, filename)
+    else:
+        return jsonify({'error': 'Invalid upload option'}), 400
+
+    api_key = os.getenv('SPOONACULAR_API_KEY')
     
-@app.route("/emails", methods=['POST'])
-def send_email():
-    try:
-            # Endpoint da API para envio de e-mails
-        endpoint = os.getenv('MAILGUN_BASE_URL') + "/messages"
+    response = requests.get(
+        f'https://api.spoonacular.com/food/images/classify?apiKey={api_key}&imageUrl={file_url}',
+    )
 
-        # Parâmetros da requisição
-        params = {
-            'from': "Excited User luciano.alcantara@ufv.br",
-            'to': ["vinicius.o.mendes@ufv.br"],
-            'subject': "Hello",
-            'text': "Testing some Mailgun awesomeness!"
-        }
+    data = json.loads(response.__dict__['_content'].decode('utf-8'))
 
-        # Autenticação com a chave de API do Mailgun
-        auth = ('api', os.getenv('MAILGUN_API_KEY'))
+    # Acessar os campos desejados
+    category = data['category']
+    probability = data['probability']
 
-        # Enviar a requisição POST para a API do Mailgun
-        response = requests.post(endpoint, auth=auth, data=params)
+    data_response = {
+       category,
+       probability
+    }
 
-        # Verificar o status da resposta
-        if response.status_code == 200:
-            print('E-mail enviado com sucesso!')
-        else:
-            print('Erro ao enviar o e-mail.')
-            print(response.text)
+    prompt = "Com base nos dados fornecidos pela API do spoonacular,"
+    prompt += "Informe qual é a possível receita do usuário,Informe em portugues o nome da receita (traduza o nome se vindo de outra lingua) e em qual categoria se enquadra"
+    prompt += f"Os dados são: {data_response}"
+    
+    # Parâmetros para a chamada da API do ChatGPT
+    params = {
+        'engine': 'text-davinci-003',
+        'prompt': prompt,
+        'max_tokens': 1000,  # Defina o número máximo de tokens na resposta
+        # Controla a aleatoriedade das respostas (0.2 é mais determinístico, 0.8 é mais criativo)
+        'temperature': 0.1,
+        'n': 1,  # Gere apenas uma resposta
+        'stop': None  # Não defina uma palavra de parada para a resposta
+    }
 
-        return jsonify({'ok': True}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-       
+    openai.api_key = os.getenv('OPENAI_KEY')
+    # Fazendo a chamada para a API do ChatGPT
+    response_open_api = openai.Completion.create(**params)
+
+    # Obtendo a descrição gerada
+    recipe = response_open_api.choices[0].text.strip()
+
+    return jsonify({ 'description':recipe,
+        'category':category,
+        'probability':probability}), 201
+
 
 if __name__ == '__main__':
     app.run(port=3333, debug=True)
