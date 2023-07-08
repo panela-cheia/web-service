a029d5d1b67eb8c265be7fb7bfd7178eea482beb
Artur Papa <artur.papa@ufv.br>
2023-07-08 12:44:55 -0300

    add(barn routes)

--- src/shared/infra/http/routes/barn_routes.py
+++ src/shared/infra/http/routes/barn_routes.py
@@ -1,8 +1,52 @@
-from flask import Blueprint, jsonify
-# from Pyro5.api import Proxy
+from flask import Blueprint, jsonify, request
+from Pyro5.api import Proxy
 
 barn_routes = Blueprint('barn', __name__)
 
-@barn_routes.route('/barn', methods=["GET"])
-def example():
-    return jsonify({'ok': 'barn routes'}), 200
+@barn_routes.route('/barn/save', methods=["POST"])
+def save_recipe():
+    barnId = request.json["barnId"]
+    recipeId = request.json["recipeId"]
+    
+    try:
+        user = Proxy("PYRONAME: adapters.save_recipe_adapter").execute(
+            barnId = barnId,
+            recipeId = recipeId
+        )
+        
+        return jsonify({'user': user}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500 
+
+@barn_routes.route('/barn/remove', methods=["PUT"])
+def remove_recipe():
+    barnId = request.json["barnId"]
+    recipeId = request.json["recipeId"]
+    
+    try:
+        user = Proxy("PYRONAME: adapters.remove_recipe_adapter").execute(
+            barnId = barnId,
+            recipeId = recipeId
+        )
+        
+        return jsonify({'user': user}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@barn_routes.route('/barn/search', methods=["GET"])
+def remove_recipe():
+    barnId = request.json["barnId"]
+    recipeName = request.json["recipeId"]
+    
+    try:
+        user = Proxy("PYRONAME: adapters.search_recipe_barn_adapter").execute(
+            barnId = barnId,
+            recipeName = recipeName
+        )
+        
+        return jsonify({'user': user}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
