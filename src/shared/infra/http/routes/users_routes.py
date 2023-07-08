d4ec20d892799fe5c3b748f73edac6ebaa7f68e1
lucianobajr <luciano.alcantara@ufv.br>
2023-07-08 12:06:19 -0300

    add

--- src/shared/infra/http/routes/users_routes.py
+++ src/shared/infra/http/routes/users_routes.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, jsonify
-# from Pyro5.api import Proxy
+from flask import Blueprint, jsonify, request
+from Pyro5.api import Proxy
 
 users_routes = Blueprint('users', __name__)
 
-@users_routes.route('/users', methods=["GET"])
-def example():
-    return jsonify({'ok': 'users routes'}), 200
+@users_routes.route('/users', methods=["POST"])
+def create_user():
+    name = request.json["name"]
+    username = request.json["username"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    try:
+        user = Proxy("PYRONAME:adapters.create_user_adapter").execute(
+            name=name,
+            username=username,
+            email=email,
+            password=password
+        )
+
+        return jsonify({'data': user}), 201
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
