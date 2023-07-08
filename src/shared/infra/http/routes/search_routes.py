2b59341a9d60f2801d52ec140e1ffe4478974291
lucianobajr <luciano.alcantara@ufv.br>
2023-07-08 13:19:36 -0300

    add(+): resolve search

--- src/shared/infra/http/routes/search_routes.py
+++ src/shared/infra/http/routes/search_routes.py
@@ -7,14 +7,14 @@ search_routes = Blueprint('search', __name__)
 def search_dive_and_users():
     search_value = request.args.get("value")
     user_id = request.args.get("user_id")
-    
+        
     try:
-        user = Proxy("PYRONAME: adapters.search_dive_and_users_adapter").execute(
+        search = Proxy("PYRONAME:adapters.search_dive_and_users_adapter").execute(
             search_value = search_value,
             user_id = user_id
         )
         
-        return jsonify({'users': user}), 200
+        return jsonify({'search': search}), 200
     
     except Exception as e:
         
